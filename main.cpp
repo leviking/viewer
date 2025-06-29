c8df3a10d5f328424a64b4b083221d9f54b5c9b6
leviking <lev@schoolstatus.com>
2025-06-29 00:59:39 -0500

    load lazier

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 
 #include <dirent.h>
 #include <string.h>
-#include <vector>
-#include <string>
 
 #define THUMBNAIL_SIZE 128
 #define PADDING 16
@@ -32,14 +30,14 @@ int main(void) {
     ImageEntry images[MAX_IMAGES];
     int imageCount = 0;
 
-    // Select folder using portable-file-dialogs
+    // Folder dialog
     auto folder = pfd::select_folder("Select a folder of images").result();
     if (folder.empty()) {
         CloseWindow();
         return 0;
     }
 
-    // Scan folder and store paths only
+    // Scan folder
     DIR *dir = opendir(folder.c_str());
     if (!dir) {
         CloseWindow();
@@ -65,7 +63,7 @@ int main(void) {
     float maxScroll = rows * (THUMBNAIL_SIZE + PADDING);
 
     while (!WindowShouldClose()) {
-        // Scroll with mouse wheel
+        // Scroll input
         float wheel = GetMouseWheelMove();
         scrollY += wheel * 30;
         if (scrollY > 0) scrollY = 0;
@@ -75,19 +73,24 @@ int main(void) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawText(TextFormat("Loaded %d images from %s", imageCount, folder.c_str()), 10, 10, 20, DARKGRAY);
+        DrawText(TextFormat("Loaded %d / %d images from %s", 
+            [&]() {
+                int n = 0;
+                for (int i = 0; i < imageCount; i++) if (images[i].loaded) n++;
+                return n;
+            }(), imageCount, folder.c_str()), 10, 10, 20, DARKGRAY);
 
+        bool loadedOneThisFrame = false;
         cols = GetScreenWidth() / (THUMBNAIL_SIZE + PADDING);
 
         for (int i = 0; i < imageCount; i++) {
             int x = (i % cols) * (THUMBNAIL_SIZE + PADDING) + PADDING;
             int y = (i / cols) * (THUMBNAIL_SIZE + PADDING) + 50 + scrollY;
 
-            // Only process if visible
             if (y + THUMBNAIL_SIZE < 0 || y > GetScreenHeight()) continue;
 
-            // Lazy load
-            if (!images[i].loaded) {
+            // Lazy load one image per frame
+            if (!images[i].loaded && !loadedOneThisFrame) {
                 Image img = LoadImage(images[i].path);
                 if (img.data != NULL) {
                     ImageResize(&img, THUMBNAIL_SIZE, THUMBNAIL_SIZE);
@@ -95,21 +98,23 @@ int main(void) {
                     UnloadImage(img);
                     images[i].loaded = true;
                 }
+                loadedOneThisFrame = true;
             }
 
             if (images[i].loaded) {
                 DrawTexture(images[i].texture, x, y, WHITE);
+            } else {
+                // Placeholder box
+                DrawRectangle(x, y, THUMBNAIL_SIZE, THUMBNAIL_SIZE, LIGHTGRAY);
+                DrawText("Loading...", x + 10, y + THUMBNAIL_SIZE / 2 - 10, 10, GRAY);
             }
         }
 
         EndDrawing();
     }
 
-    // Unload only loaded textures
     for (int i = 0; i < imageCount; i++) {
-        if (images[i].loaded) {
-            UnloadTexture(images[i].texture);
-        }
+        if (images[i].loaded) UnloadTexture(images[i].texture);
     }
 
     CloseWindow();
